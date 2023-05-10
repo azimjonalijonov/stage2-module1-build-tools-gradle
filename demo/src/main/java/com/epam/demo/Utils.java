070150f98588195249c7808ace24076cb1880634
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-05-10 14:02:50 +0500

    Update Utils.java

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,21 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        if (args.isEmpty()) {
+            return false;
+        }
+        for (String string :
+                args) {
+            if (!StringUtils.isPositiveNumber(string)) {
+                return false;
+            }
+        }
+        return true;
     }
 }

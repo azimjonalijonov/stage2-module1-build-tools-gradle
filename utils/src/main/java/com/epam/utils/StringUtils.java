708db092d01d48a9ad053993d99cb05751f98f9f
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-05-10 14:04:41 +0500

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,17 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        //here magic will happen
+        if (!NumberUtils.isNumber(str)) {
+            return false;
+        }
+        return Double.parseDouble(str) > 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-55.1");
     }
 }
